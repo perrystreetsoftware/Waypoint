e527fb7f7de1f5bd2ee2864614bf6272feb63f65
Matheus Luz <matheus@perrystreet.com>
2026-09-14 10:43:46 -0300

    Expose selected state through label builder

--- Sources/Waypoint/Models/RootTab.swift
+++ Sources/Waypoint/Models/RootTab.swift
@@ -5,17 +5,27 @@ public typealias NavigatorTab = AnyHashable
 public struct RootTab {
     let id: NavigatorTab
     let view: AnyView
-    let label: AnyView?
+    private let labelBuilder: (_ isSelected: Bool) -> AnyView
 
-    public init(id: some Hashable, @ViewBuilder view: () -> some View) {
-        self.id = id
-        self.view = AnyView(view())
-        self.label = nil
+    public init(
+        id: some Hashable,
+        @ViewBuilder label: @escaping () -> some View,
+        @ViewBuilder view: () -> some View
+    ) {
+        self.init(id: id, label: { _ in label() }, view: view)
     }
 
-    public init(id: some Hashable, @ViewBuilder label: () -> some View, @ViewBuilder view: () -> some View) {
+    public init(
+        id: some Hashable,
+        @ViewBuilder label: @escaping (_ isSelected: Bool) -> some View,
+        @ViewBuilder view: () -> some View
+    ) {
         self.id = id
         self.view = AnyView(view())
-        self.label = AnyView(label())
+        self.labelBuilder = { AnyView(label($0)) }
+    }
+
+    func label(isSelected: Bool) -> AnyView {
+        labelBuilder(isSelected)
     }
 }
